14658199f51e1325d557ff71a602b4f3e5dc32be
wagnermateus <wagnermateus7@gmail.com>
2023-02-10 12:06:28 +0100

    correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
-import "./App.css";
-function App() {
+
+export function App() {
   return (
     <div className="App">
       <Navbar />
@@ -9,5 +9,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
